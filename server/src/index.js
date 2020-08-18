5407f5ead50d5929ccae4d8e823e9913ed40efab
Mateo Velilla <mvelilla@bostonprojectllc.com>
2020-08-18 18:50:34 -0500

    Added new route

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,17 @@
+const path = require('path');
 const Koa = require('koa');
+const cors = require('@koa/cors');
+const bodyParser = require('koa-bodyparser')
+const dotenv = require('dotenv');
 const app = new Koa();
+app.use(cors());
+app.use(bodyParser());
+dotenv.config({path: path.resolve(__dirname, '../.env')});
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+// Import Routes
+const ProductRouter = require(path.resolve(__dirname, './routes/productRouter'))
+
+app.use(ProductRouter().routes())
+
 
 app.listen(3000);
