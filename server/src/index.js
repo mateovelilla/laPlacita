2cc552c257f94060139e06ba7811491268ef6a15
Mateo Velilla <mvelilla@bostonprojectllc.com>
2020-08-18 20:13:10 -0500

    Refactoring build connection to mongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,36 @@
 const path = require('path');
 const Koa = require('koa');
 const cors = require('@koa/cors');
-const bodyParser = require('koa-bodyparser')
+const bodyParser = require('koa-bodyparser');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = new Koa();
 app.use(cors());
 app.use(bodyParser());
-dotenv.config({path: path.resolve(__dirname, '../.env')});
 
-// Import Routes
+// GET THE ENVIRONMENT VARIABLES
+dotenv.config({path: path.resolve(__dirname, '../.env')});
+const { MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT, MONGODB_DBNAME} = process.env
+
+// IMPORT ROUTES
 const ProductRouter = require(path.resolve(__dirname, './routes/productRouter'))
 
-app.use(ProductRouter().routes())
+// BUILD DATABASE URL
+const url = `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DBNAME}`;
 
+// START CONNECTION TO MONGODB
+
+mongoose.connect(url, {
+  auth: {
+    authSource: 'admin'
+  },
+  "user": MONGODB_USERNAME,
+  "pass": MONGODB_PASSWORD,
+  useUnifiedTopology: true,
+  useNewUrlParser: true 
+});
+
+// ROUTES INJECTION
+app.use(ProductRouter().routes());
 
 app.listen(3000);
