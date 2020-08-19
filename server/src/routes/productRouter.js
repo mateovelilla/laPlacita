619add2cf6053d248c87394585ef07f7ff5ef83c
Mateo Velilla <mvelilla@bostonprojectllc.com>
2020-08-18 20:14:26 -0500

    Injected model and controller to productRouter

--- server/src/routes/productRouter.js
+++ server/src/routes/productRouter.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const Router = require('@koa/router');
+const ProductModel = require(path.resolve(__dirname, '../models/Product'))
+const ProductController = require(path.resolve(__dirname,'../controllers/productController')) 
 module.exports = function () {
   const router = new Router();
+  const productController = new ProductController(ProductModel)
   router.get('/products', async (ctx, next) => {
+    const products = await productController.find({})
     ctx.status = 200;
     ctx.body = {
-      message: 'products'
+      products
     }
   })
   return router;
