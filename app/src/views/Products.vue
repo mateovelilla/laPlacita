3a4032ef242235ba0096696c63ec7940529b40b6
Mateo Velilla <mvelilla@bostonprojectllc.com>
2020-08-19 23:21:42 -0500

    Added views fake to the products

---
+++ app/src/views/Products.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col cols="3" v-for="product in products" :key="product.id">
+      <Product :product="product"/>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import API from '../services/api'
+import Product from '../components/product'
+export default {
+  components: {
+    Product
+  },
+  data () {
+    return {
+      products: []
+    }
+  },
+  async mounted () {
+    const { data: { products } } = await API.getProducts()
+    this.products = products
+  }
+}
+</script>
