83a286d667d0595a16058fe320abda0ed7a56713
Mateo Velilla <mvelilla@bostonprojectllc.com>
2020-08-19 23:16:33 -0500

    Added fonts and custom properties options

--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -1,25 +1,25 @@
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-	icons: {
-		iconfont: 'md',
-	},
-	theme: {
-		options: {
-			customProperties: true,
-		},
+  icons: {
+    iconfont: 'md'
+  },
+  theme: {
+    options: {
+      customProperties: true
+    },
     themes: {
-			light: {
-				primary: '#37475A',
-				secondary: '#F2C84B',
-				error: '#D96941',
-				light: '#8C614C',
-				btn: '#8B7B70'
-			}
-		}
-	}
-});
+      light: {
+        primary: '#37475A',
+        secondary: '#F2C84B',
+        error: '#D96941',
+        light: '#8C614C',
+        btn: '#8B7B70'
+      }
+    }
+  }
+})
---
+++ app/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Products from '../views/Products.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    children: [
+      {
+        path: '/',
+        name: 'products',
+        component: Products
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
