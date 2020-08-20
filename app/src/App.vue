6910686047588ef8cb9661018ecfa0de12e38e99
Mateo Velilla <mvelilla@bostonprojectllc.com>
2020-08-19 23:09:45 -0500

    Refactoring app.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,60 +1,11 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      flat
-      color="secondary"
-    >
-      <v-row>
-        <v-col cols="2">
-          <v-img
-            alt="Vuetify Logo"
-            class="mr-2"
-            contain
-            :src="require('@/assets/logo.png')"
-            transition="scale-transition"
-            width="100"
-          />
-        </v-col>
-        <v-col cols="8" class="pl-0 d-flex align-center">
-          <v-text-field
-            light
-            append-icon="search"
-            hide-details
-            solo
-            flat
-          >
-            <template v-slot:prepend-inner>
-              <v-select
-                label="Category"
-                hide-details
-                flat
-                color="white"
-                light
-              ></v-select>
-            </template>
-          </v-text-field>
-        </v-col>
-      </v-row>
-    </v-app-bar>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  name: 'App'
+}
 </script>
+<style>
+</style>
